7e952f6f3ab9f645f112fa10969e1943cfeefee3
stinodes <stijn.tytgat@ixor.be>
2019-02-11 11:38:02 +0100

    * added build folders to gitignore * added basepath based on env

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import { ThemeProvider } from 'emotion-theming'
 import config from './config'
 import { Register } from './Register'
 import { ResetPassword } from './ResetPassword'
+import { isDev } from './utils'
 
+const basepath = isDev() ? '/user-registration-ui' : ''
 const App = () => (
   <ThemeProvider theme={config.theme}>
-    <Router id="router" basepath="/user-registration-ui">
-      <Redirect noThrow from="/" to="/register" />
+    <Router id="router" basepath={basepath}>
+      <Redirect noThrow={isDev()} from="/" to={`${basepath}/register`} />
       <Register path="register" />
       <ResetPassword path="reset-password" />
     </Router>
